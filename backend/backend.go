942027db6631ee0cce4420986414422321cab4ae
e.valeev <e.valeev@semrush.com>
2020-05-03 07:15:10 +0300

    feat(backend): add go.mod and server

---
+++ backend/backend.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func handleAPI(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w, r)
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	http.HandleFunc("/api/", handleAPI)
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+
+	fmt.Println("Server is running on port: " + port)
+	http.ListenAndServe(":"+port, nil)
+}
---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/emil14/remembro
+
+go 1.14
