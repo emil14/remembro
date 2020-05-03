b287bc21253aa171abe3e624b36da6a01381e61f
e.valeev <e.valeev@semrush.com>
2020-05-03 07:16:53 +0300

    feat: remove all

--- frontend/build/webpack.prod.js
+++
@@ -1,6 +0,0 @@
-const webpackMerge = require("webpack-merge");
-const commonWebpackConfig = require("./webpack.common");
-
-module.exports = webpackMerge(commonWebpackConfig, {
-  mode: "production"
-});
