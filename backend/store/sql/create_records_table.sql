1ef1715c169de5a35109f959f12851327ff5e20a
emil14 <emil.musician@gmail.com>
2020-06-14 14:03:40 +0300

    feat(backend:sql): add tags table

--- backend/store/sql/create_records_table.sql
+++ backend/store/sql/create_records_table.sql
@@ -1,2 +1,6 @@
 DROP TABLE IF EXISTS records;
-CREATE TABLE records (id SERIAL, content VARCHAR(255))
+CREATE TABLE records (
+    id SERIAL,
+    content VARCHAR(255),
+    PRIMARY KEY (id)
+)
---
+++ backend/store/sql/create_tags_table.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS tags;
+CREATE TABLE tags (
+    id SERIAL,
+    name VARCHAR(32),
+    parent_id: INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (parent_id) REFERENCES tags(id)
+)
