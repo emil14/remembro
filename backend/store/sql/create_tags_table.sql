276f8dcc118f202bd41e637701246c5055296b9e
emil14 <emil.musician@gmail.com>
2020-06-14 14:18:11 +0300

    fix(backend:sql): fix create tags table

--- backend/store/sql/create_tags_table.sql
+++ backend/store/sql/create_tags_table.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS tags;
 CREATE TABLE tags (
     id SERIAL,
     name VARCHAR(32),
-    parent_id: INT,
+    parent_id INT,
     PRIMARY KEY (id),
     FOREIGN KEY (parent_id) REFERENCES tags(id)
 )
