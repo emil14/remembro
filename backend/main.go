ed9046cf702487f2828c97c6e6897a278133ae9b
emil14 <emil.musician@gmail.com>
2020-06-23 19:43:56 +0300

    refactor(backend): move to mux, add logger middlware

--- backend/main.go
+++ backend/main.go
@@ -8,105 +8,100 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/gorilla/mux"
+
 	"github.com/emil14/remembro/conf"
 	"github.com/emil14/remembro/models"
 )
 
-func recordsHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		rr, err := models.GetRecords()
-		if err != nil {
-			http.Error(w, http.StatusText(500), 500)
-			fmt.Println(err)
-			return
-		}
-		resp, err := json.Marshal(rr)
-		if err != nil {
-			fmt.Println(err)
-			http.Error(w, http.StatusText(500), 500)
-			return
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Write(resp)
-	case "POST":
-		type createdRecord struct {
-			Content string `json:"content"`
-			TagsIds []int  `json:"tagsIds"`
-		}
-		var c createdRecord
-		if err := json.NewDecoder(r.Body).Decode(&c); err != nil {
-			fmt.Println(err)
-			http.Error(w, http.StatusText(500), 500)
-			return
-		}
-		if err := models.CreateRecord(c.Content, time.Now(), c.TagsIds); err != nil {
-			fmt.Println(err)
-			http.Error(w, http.StatusText(500), 500)
-			return
-		}
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Write([]byte("Record created")) // TODO return created record
-	default:
-		http.Error(w, http.StatusText(405), 405)
-	}
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.RequestURI)
+		next.ServeHTTP(w, r)
+	})
 }
 
-func tagsHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		tt, err := models.GetTags()
-		if err != nil {
-			http.Error(w, http.StatusText(500), 500)
-			fmt.Println(err)
-			return
-		}
-		resp, err := json.Marshal(tt)
-		if err != nil {
-			fmt.Println(err)
-			http.Error(w, http.StatusText(500), 500)
-			return
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Write(resp)
-	case "POST":
-		buf := new(bytes.Buffer)
-		buf.ReadFrom(r.Body)
-		if err := models.CreateTag(buf.String()); err != nil {
-			fmt.Println(err)
-			http.Error(w, http.StatusText(500), 500)
-			return
-		}
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Write([]byte("Tag created")) // TODO return created record
-	default:
-		http.Error(w, http.StatusText(405), 405)
+func getRecords(w http.ResponseWriter, r *http.Request) {
+	rr, err := models.GetRecords()
+	if err != nil {
+		http.Error(w, http.StatusText(500), 500)
+		fmt.Println(err)
+		return
 	}
-}
-
-func clientErrorsHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		http.Error(w, http.StatusText(405), 405)
+	resp, err := json.Marshal(rr)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, http.StatusText(500), 500)
+		return
 	}
-	// TODO save error to log
+	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Write([]byte("Error saved"))
+	w.Write(resp)
+}
+
+func createRecord(w http.ResponseWriter, r *http.Request) {
+	type createdRecord struct {
+		Content string `json:"content"`
+		TagsIds []int  `json:"tagsIds"`
+	}
+	var c createdRecord
+	if err := json.NewDecoder(r.Body).Decode(&c); err != nil {
+		fmt.Println(err)
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+	if err := models.CreateRecord(c.Content, time.Now(), c.TagsIds); err != nil {
+		fmt.Println(err)
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Write([]byte("Record created")) // TODO return created record
+}
+
+func getTags(w http.ResponseWriter, r *http.Request) {
+	tt, err := models.GetTags()
+	if err != nil {
+		http.Error(w, http.StatusText(500), 500)
+		fmt.Println(err)
+		return
+	}
+	resp, err := json.Marshal(tt)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Write(resp)
+}
+
+func createTag(w http.ResponseWriter, r *http.Request) {
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(r.Body)
+	if err := models.CreateTag(buf.String()); err != nil {
+		fmt.Println(err)
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Write([]byte("Tag created")) // TODO return created
 }
 
 func main() {
 	closeDB := models.InitDB()
-	defer func() {
-		if err := closeDB(); err != nil {
-			log.Fatal(err)
-		}
-	}()
+	defer closeDB()
 
-	http.HandleFunc("/api/records", recordsHandler)
-	http.HandleFunc("/api/tags", tagsHandler)
-	http.HandleFunc("/api/clienterrors", clientErrorsHandler)
+	r := mux.NewRouter()
+	r.Use(loggingMiddleware)
+	r.HandleFunc("/api/records", getRecords).Methods("GET")
+	r.HandleFunc("/api/records", createRecord).Methods("POST")
+	r.HandleFunc("/api/tags", getTags).Methods("GET")
+	r.HandleFunc("/api/tags", createTag).Methods("POST")
 
-	fmt.Println("Server is running on port: " + conf.ServerPort)
-	log.Fatal(http.ListenAndServe(":"+conf.ServerPort, nil))
+	fmt.Println("---\nServer running on port: " + conf.ServerPort + "\n---")
+	if err := http.ListenAndServe(":"+conf.ServerPort, r); err != nil {
+		defer log.Fatal(err)
+	}
 }
--- backend/models/db.go
+++ backend/models/db.go
@@ -12,12 +12,16 @@ import (
 
 var db *sql.DB
 
-func InitDB() func() error {
+func InitDB() func() {
 	connStr := fmt.Sprintf("user=%s password=%s dbname=%s port=%s", conf.DBUser, conf.DBPassword, conf.DBName, conf.DBPort)
 	var err error
 	db, err = sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err)
 	}
-	return db.Close
+	return func() {
+		if err := db.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}
 }
